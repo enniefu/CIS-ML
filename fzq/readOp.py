84c2e2366bd4b5b9bf519e46907fec689cd65feb
enniefu <enniefu@gmail.com>
2019-07-21 15:45:33 +0800

    修复了读取数据时产生的小错误

--- fzq/readOp.py
+++ fzq/readOp.py
@@ -1,33 +1,50 @@
 import re
 
-def readOp(url):
+def readOpcal(url):
+
+
+    '''
+    注意，为了方便进行数据处理，
+    我删除了本文件的第一行。
+    后期如果数据不一样的时候，记得修改。
+    '''
+
+
     with open(url,'r') as f:
         data = []
+        lines = f.readline() #读第一行，删掉表头
         while True:
-            i=0
             lines = f.readline()  # 整行读取数据
-            i=i+1
             if not lines:
                 break
-            if(i!=0):
-                dataline=[]
-                lines = f.readline()
-                texts = lines.split(' ')
-                # print(text)
-                # break
-                for text in texts:
-                    if text==' ' or text=='':
-                        continue
-                    else:
-                        result = re.match(r"[0-9]*.?[0-9]*", text)
-                        try:
-                            dataline.append(float(result.group()))
-                        except:
-                            print(result)
-                process_dataline=[]
-                for i in range(len(dataline)):
-                    if i not in (0,1,2,4,6,8,10,12,14):
-                        process_dataline.append(dataline[i])
-                data.append(process_dataline)
+
+            #处理一行的数据
+            dataline=[]
+            texts = lines.split(' ')
+
+            #匹配本行过滤字段中无意义的符号
+            for text in texts:
+                if text==' ' or text=='':
+                    continue
+                else:
+                    result = re.match(r"[0-9]*.?[0-9]*", text)
+                    try:
+                        dataline.append(float(result.group()))
+                    except:
+                        print(result)
+
+            #删除无意义的字段
+            process_dataline=[]
+            for i in range(len(dataline)):
+                if i not in (0,1,2,4,6,8,10,12,14):
+                    process_dataline.append(dataline[i])
+            data.append(process_dataline)
+
         return data
 
+if __name__ == '__main__':
+    url=r"C:\Users\ENNIE\OneDrive\CIS\gsod_2019\010010-99999-2019.op"
+    texts=readOpcal(url)
+    for i in texts:
+        print(i)
+    print(len(texts))
